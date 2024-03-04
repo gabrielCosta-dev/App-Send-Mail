21bbffd3fe68ff0557cd47e6702b82ba9f9ff58b
Gabriel Lima <lima20gabriel@gmail.com>
2024-03-04 16:01:00 -0300

    Criando class Mensagem e alguns ajustes na index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
-    	<title>App Mail Send</title>
+    	<title>App Send Mail</title>
 
     	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
@@ -21,20 +21,20 @@
       			<div class="col-md-12">
   				
 					<div class="card-body font-weight-bold">
-						<form>
+						<form action="processa_envio.php" method="post">
 							<div class="form-group">
 								<label for="para">Para</label>
-								<input type="text" class="form-control" id="para" placeholder="joao@dominio.com.br">
+								<input type="text" class="form-control" id="para" placeholder="joao@dominio.com.br" name="para">
 							</div>
 
 							<div class="form-group">
 								<label for="assunto">Assunto</label>
-								<input type="text" class="form-control" id="assunto" placeholder="Assunto do e-mail">
+								<input type="text" class="form-control" id="assunto" placeholder="Assunto do e-mail" name="assunto">
 							</div>
 
 							<div class="form-group">
 								<label for="mensagem">Mensagem</label>
-								<textarea class="form-control" id="mensagem"></textarea>
+								<textarea class="form-control" id="mensagem" name="mensagem"></textarea>
 							</div>
 
 							<button type="submit" class="btn btn-primary btn-lg">Enviar Mensagem</button>
---
+++ processa_envio.php
@@ -0,0 +1,40 @@
+<?php
+    #print_r($_POST);
+
+    class Mensagem {
+        private $para = null;
+        private $assunto = null;
+        private $mensagem = null;
+
+        public function __get($atributo){
+            return $this->$atributo;
+        }
+
+        public function __set($atributo, $valor){
+            $this->$atributo = $valor;
+        }
+
+        public function mensagemValida(){
+            if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    $mensagem = new Mensagem();
+    $mensagem->__set('para', $_POST['para']);
+    $mensagem->__set('assunto', $_POST['assunto']);
+    $mensagem->__set('mensagem', $_POST['mensagem']);
+
+    echo '<pre>';
+    print_r($mensagem);
+    echo '</pre>';
+
+    if ($mensagem->mensagemValida()) {
+        echo 'Mensagem válida';
+    } else {
+        echo 'Mensagem inválida';
+    }
+?>
